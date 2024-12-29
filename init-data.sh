4478d479c30081d85ea4e05dc4413190b9291858
Aldair Meneses <aldair.meneses@protonmail.com>
2024-12-29 14:30:41 -0300

    fix: identation format

--- init-data.sh
+++ init-data.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -e;
 
-
 if [ -n "${POSTGRES_NON_ROOT_USER:-}" ] && [ -n "${POSTGRES_NON_ROOT_PASSWORD:-}" ]; then
 	psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
 		CREATE USER ${POSTGRES_NON_ROOT_USER} WITH PASSWORD '${POSTGRES_NON_ROOT_PASSWORD}';
